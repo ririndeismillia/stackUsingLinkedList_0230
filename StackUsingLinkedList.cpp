2e9d8b2c5554c882e2c2bd0101d27770827eb6e5
ririndeismillia <ririndeismillia@gmail.com>
2026-05-12 11:20:24 +0700

    default

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -121,4 +121,11 @@ int main()
         case 4:
             cout << "Exiting program." <<endl;
             break;
-        
+        default:
+            cout << "invalid choice. Try again." <<endl;
+            break;        
+        }
+        cout << endl;
+    }
+    return 0;
+};
