07ad2ae5cd7122e5b02e2eecbca086f63b4782ec
ririndeismillia <ririndeismillia@gmail.com>
2026-05-12 11:10:07 +0700

    peel

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -47,4 +47,11 @@ public:
     top = top->next;
     cout << "Popped value: " << top->data << endl;
    }
+   //peel/top operation Retrive the value of the value of the topset element witchout removing
+   void peek()
+   {
+    if (top == NULL)
+    {
+        cout << "List is empty." << endl;
+    }
 };
