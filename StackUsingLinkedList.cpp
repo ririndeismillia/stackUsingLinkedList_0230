c070067903add099284c3ed13ff7b299eb3f40f3
ririndeismillia <ririndeismillia@gmail.com>
2026-05-12 11:11:00 +0700

    peel2

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -54,4 +54,15 @@ public:
     {
         cout << "List is empty." << endl;
     }
+    else
+    {
+        Node *current = top;
+        while (current != NULL)
+        {
+            cout << current->data <<""<< endl;
+            current = current->next;
+        }
+        cout << endl;
+
+    }
 };
