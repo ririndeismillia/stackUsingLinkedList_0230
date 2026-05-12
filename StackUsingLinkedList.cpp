43e4104b6d171ec26246d75a23e2b2b9fb610ecc
ririndeismillia <ririndeismillia@gmail.com>
2026-05-12 11:03:36 +0700

    node class representating a single node in the linked list

---
+++ StackUsingLinkedList.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+//node class representating a single node in the linked list
+
+class Node
+{
+public:
+   int data;
+   Node *next;
+
+   Node()
+    {
+        next = NULL;
+    }
+};
+
